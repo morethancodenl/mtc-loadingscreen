560725cb9c2e6e8f5eac563bd6d1ca0d0b07db2a
Mitchel <324712@student.mboutrecht.nl>
2023-12-20 18:11:45 +0100

    feat! 'is loading' to typeSentence in js.

---
+++ web/js/app.js
@@ -0,0 +1,23 @@
+async function typeSentence(sentence, eleRef, delay = 100) {
+    const letters = sentence.split("");
+    let i = 0;
+    $(eleRef).empty(); // Leeg het element voordat een nieuwe zin wordt getypt
+    while (i < letters.length) {
+        await waitForMs(delay);
+        $(eleRef).append(letters[i]);
+        i++;
+    }
+    return;
+}
+
+function waitForMs(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function repeatTypeSentence() {
+    typeSentence("is loading...", "#sentence");
+}
+
+setInterval(repeatTypeSentence, 15000);
+
+repeatTypeSentence();
